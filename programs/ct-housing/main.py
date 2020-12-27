c9ea04966fe8122523c72745632f298fd5eca16b
ajarombek <ajarombek95@gmail.com>
2020-12-26 22:17:57 -0500

    data passed to ML algorithm

--- programs/ct-housing/main.py
+++ programs/ct-housing/main.py
@@ -6,8 +6,18 @@ Date: 12/25/2020
 
 import urllib.request
 import os
+import time
+from datetime import datetime
+from typing import Tuple
 
 import pandas as pd
+import scipy.sparse
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # https://data.ct.gov/Housing-and-Development/Real-Estate-Sales-2001-2018/5mzw-sjtu
 HOUSING_DATASET_URL = "https://data.ct.gov/api/views/5mzw-sjtu/rows.csv?accessType=DOWNLOAD"
@@ -36,9 +46,69 @@ def load_ct_housing_data() -> pd.DataFrame:
 
 
 def clean_ct_housing_data(data: pd.DataFrame) -> pd.DataFrame:
-    pass
+    """
+    Clean the Connecticut housing data by removing null values and mapping dates to their corresponding unix values.
+    :param data: The housing data set.
+    :return: The cleaned data.
+    """
+    clean_data = data.drop(['Remarks', 'Address', 'SerialNumber', 'NonUseCode'], axis=1)
+    clean_data = clean_data[~clean_data['DateRecorded'].isnull()]
+    clean_data = clean_data[~clean_data['AssessedValue'].isnull()]
+    clean_data = clean_data[~clean_data['SaleAmount'].isnull()]
+    clean_data = clean_data[~clean_data['SalesRatio'].isnull()]
+
+    def convert_to_unix(x: str) -> int:
+        ts = datetime.strptime(x, "%m/%d/%Y")
+        return int(time.mktime(ts.timetuple()))
+
+    clean_data['DateRecorded'] = clean_data['DateRecorded'].apply(convert_to_unix)
+    return clean_data
+
+
+def split_labels_and_data(data: pd.DataFrame) -> Tuple[pd.Series, pd.DataFrame]:
+    """
+    Split the housing data into two sets.  The first set is the label (the correct answer to the ML problem).  In this
+    scenario, the label is the price that the house was sold for.  The second set is the data that is fed to the machine
+    learning algorithm.  It is the entire initial dataset minus the label.
+    :param data: The housing data set.
+    :return: Label data (tuple index 0) and the training and testing data (tuple index 1).
+    """
+    return data['SaleAmount'].copy(), data.drop('SaleAmount', axis=1)
+
+
+def transform_and_encode_ct_housing_data(data: pd.DataFrame) -> scipy.sparse:
+    """
+    Transform the data so that it performs better as a machine learning training model.
+    :param data: The housing data (without the label)
+    :return: The data which is passed to the ML algorithm.
+    """
+    num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler())
+    ])
+
+    cat_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='constant', fill_value='Unknown')),
+        ('encoder', OneHotEncoder())
+    ])
+
+    full_pipeline = ColumnTransformer([
+        ('num', num_pipeline, ['ListYear', 'DateRecorded', 'AssessedValue', 'SalesRatio', 'NumberOfBuildings']),
+        ('cat', cat_pipeline, ['Town', 'PropertyType', 'ResidentialType'])
+    ])
+
+    return full_pipeline.fit_transform(data)
+
+
+def train_ct_housing_model(prepared_data: scipy.sparse, labels: pd.Series) -> LinearRegression:
+    lin_reg = LinearRegression()
+    lin_reg.fit(prepared_data, labels)
+    return lin_reg
 
 
 if __name__ == '__main__':
     fetch_ct_housing_data()
-    housing_data = load_ct_housing_data()
+    initial_housing_data = load_ct_housing_data()
+    train_set, test_set = train_test_split(initial_housing_data, test_size=0.2, random_state=42)
+    cleaned_housing_data = clean_ct_housing_data(train_set)
+    housing_data_labels, housing_data = split_labels_and_data(cleaned_housing_data)
